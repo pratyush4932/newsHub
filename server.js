21879fb477a643356188251745cd0995cfed3f95
Yush <86536877+pratyush4932@users.noreply.github.com>
2023-02-06 11:45:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const axios = require('axios')
 const app = express()
-const port = 8080
+const port = 3000
 
 app.use(express.static('public'));
 
