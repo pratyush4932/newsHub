bc2927c295147f93aaf4fff6ea3667ac03007ba5
Yush <86536877+pratyush4932@users.noreply.github.com>
2023-02-06 11:36:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const axios = require('axios')
 const app = express()
 const port = 8080
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname) } )
 })
